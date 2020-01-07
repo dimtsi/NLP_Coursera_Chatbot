6f5ceea7e8a2f8d919b48cc1cb2540b45abc598d
Dimitrios Tsimpoukis <tsiboukisdim@gmail.com>
2020-01-07 12:01:27 +0100

    all

--- dialogue_manager.py
+++ dialogue_manager.py
@@ -31,18 +31,20 @@ class ThreadRanker(object):
         question_vec = question_to_vec(question, self.word_embeddings, dim=self.embeddings_dim)
         #### YOUR CODE HERE ####
         # best_thread = pairwise_distances_argmin(question_vec.reshape(1, -1), thread_embeddings, metric='cosine')[0]
+
+        # Due to memory errors we load the threads in chunks
         scores_list = []
         k = 10
         n = int(len(thread_ids) / k)
         for i in range(k):
             print(i)
             if i == k - 1:
-                break
+                break # Due to memory error (AWS 1GB Free Tier) we do not include the last chunk of threads
                 # best_thread, dist = pairwise_distances_argmin_min(question_vec.reshape((1, self.embeddings_dim)),
                 #                                                   thread_embeddings[i * n:, :], metric='cosine')
             else:
                 best_thread, dist = pairwise_distances_argmin_min(question_vec.reshape((1, self.embeddings_dim)),
-                                                                  thread_embeddings[i * n:(i + 1) * n, :],
+                                                                  thread_embeddings[i * n: (i + 1) * n, :],
                                                                   metric='cosine')
 
             scores_list.append({'thread': i * n + best_thread[0], 'dist': dist[0]})
@@ -50,70 +52,6 @@ class ThreadRanker(object):
         df = pd.DataFrame(scores_list).sort_values(by='dist')
         best_thread = int(df.iloc[0]['thread'])
         return thread_ids[best_thread]
-
-# class DialogueManager(object):
-#     def __init__(self, paths):
-#         print("Loading resources...")
-#         # Create chitchatbot
-#         self.chitchatbot = self.create_chitchat_bot()
-#         # Intent recognition:
-#         self.intent_recognizer = unpickle_file(paths['INTENT_RECOGNIZER'])
-#         self.tfidf_vectorizer = unpickle_file(paths['TFIDF_VECTORIZER'])
-#
-#         self.ANSWER_TEMPLATE = 'I think its about %s\nThis thread might help you: https://stackoverflow.com/questions/%s'
-#
-#         # Goal-oriented part:
-#         self.tag_classifier = unpickle_file(paths['TAG_CLASSIFIER'])
-#         self.thread_ranker = ThreadRanker(paths)
-#
-#     def create_chitchat_bot(self):
-#         """Initializes self.chitchat_bot with some conversational model."""
-#
-#         # Hint: you might want to create and train chatterbot.ChatBot here.
-#         # It could be done by creating ChatBot with the *trainer* parameter equals
-#         # "chatterbot.trainers.ChatterBotCorpusTrainer"
-#         # and then calling *train* function with "chatterbot.corpus.english" param
-#
-#         ########################
-#         #### YOUR CODE HERE ####
-#         ########################
-#         chatbot = ChatBot('dimtsi chitchatbot')
-#         trainer = ChatterBotCorpusTrainer(chatbot, show_training_progress=False)
-#         trainer.train("chatterbot.corpus.english")
-#         return chatbot
-#
-#     def generate_answer(self, question):
-#         """Combines stackoverflow and chitchat parts using intent recognition."""
-#
-#         # Recognize intent of the question using `intent_recognizer`.
-#         # Don't forget to prepare question and calculate features for the question.
-#
-#         #### YOUR CODE HERE ####
-#         prepared_question = text_prepare(question)
-#         #### YOUR CODE HERE ####
-#         features = self.tfidf_vectorizer.transform([prepared_question])  # needs list
-#         print(features.shape)
-#         #### YOUR CODE HERE ####
-#         intent = self.intent_recognizer.predict(features)
-#
-#         # Chit-chat part:
-#         if intent == 'dialogue':
-#             # Pass question to chitchat_bot to generate a response.
-#             #### YOUR CODE HERE ####
-#             response = self.chitchatbot.get_response(question)
-#             return response
-#
-#         # Goal-oriented part:
-#         else:
-#             # Pass features to tag_classifier to get predictions.
-#             #### YOUR CODE HERE ####
-#             tag = self.tag_classifier.predict(features)[0]
-#             print("tag: {}".format(tag))
-#
-#             # Pass prepared_question to thread_ranker to get predictions.
-#             #### YOUR CODE HERE ####
-#             thread_id = self.thread_ranker.get_best_thread(prepared_question, tag)
-#             return self.ANSWER_TEMPLATE % (tag, thread_id)
 
 
 class DialogueManager(object):
